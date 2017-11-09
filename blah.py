56f41d37565605af0a817c8c17cdb01d1cdae8e0
ericlok <ericlok@gmail.com>
2017-11-08 22:52:49 -0500

    put back change #2

--- blah.py
+++ blah.py
@@ -4,9 +4,55 @@ import os
 import numpy as np
 from optparse import OptionParser
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+
+def get_pipeline(crop=False):
+    model = Sequential()
+    # Cropping
+    if crop:
+        model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(3, 160, 320)))
+    # Normalize
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+    # 1
+    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(MaxPooling2D())
+    # 2
+    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(MaxPooling2D())
+    # 3
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
+def get_nvidia_pipeline(crop=False):
+    model = Sequential()
+    # Cropping
+    if crop:
+        model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(3, 160, 320)))
+        # model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    # Normalize
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+    # model.add(Dropout(0.5))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    # model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+    return model
 
 def run(location):
     if location == 'home':
@@ -48,24 +94,8 @@ def run(location):
     # X_train = np.concatenate([images, augmented_images])
     # y_train = np.concatenate([measurements, augmented_measurements])
 
-    model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-
-    # 1
-    model.add(Convolution2D(6, 5, 5, activation='relu'))
-    model.add(MaxPooling2D())
-    # 2
-    model.add(Convolution2D(6, 5, 5, activation='relu'))
-    model.add(MaxPooling2D())
-    # 3
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(1))
-
-    model.compile(loss='mse', optimizer='adam')
+    model = get_pipeline(crop=False)
     model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
-
     model.save('model.h5')
 
 if __name__ == '__main__':
