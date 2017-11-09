1a6b7ec3a6bed8f9d6d30a1b9c68cad08285c138
ericlok <ericlok@gmail.com>
2017-11-08 23:08:00 -0500

    put back change #3

--- blah.py
+++ blah.py
@@ -8,52 +8,6 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-def get_pipeline(crop=False):
-    model = Sequential()
-    # Cropping
-    if crop:
-        model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(3, 160, 320)))
-    # Normalize
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-    # 1
-    model.add(Convolution2D(6, 5, 5, activation='relu'))
-    model.add(MaxPooling2D())
-    # 2
-    model.add(Convolution2D(6, 5, 5, activation='relu'))
-    model.add(MaxPooling2D())
-    # 3
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(1))
-
-    model.compile(loss='mse', optimizer='adam')
-    return model
-
-def get_nvidia_pipeline(crop=False):
-    model = Sequential()
-    # Cropping
-    if crop:
-        model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(3, 160, 320)))
-        # model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-    # Normalize
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-    # model.add(Dropout(0.5))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    # model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dense(10))
-    model.add(Dense(1))
-
-    model.compile(loss='mse', optimizer='adam')
-    return model
-
 def run(location):
     if location == 'home':
         PATH = r'/Users/ericlok/Downloads'
@@ -94,7 +48,25 @@ def run(location):
     # X_train = np.concatenate([images, augmented_images])
     # y_train = np.concatenate([measurements, augmented_measurements])
 
-    model = get_pipeline(crop=False)
+    model = Sequential()
+    # Cropping
+    # if crop:
+    #     model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(3, 160, 320)))
+    # Normalize
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+    # 1
+    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(MaxPooling2D())
+    # 2
+    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(MaxPooling2D())
+    # 3
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
     model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
     model.save('model.h5')
 
