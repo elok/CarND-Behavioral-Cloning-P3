4b0e05bc0a139bcc5dd26a1345c1438796d7ef69
ericlok <ericlok@gmail.com>
2017-11-11 15:42:07 -0500

    added comments, training_data_backwards, training_data_correction

--- model.py
+++ model.py
@@ -6,11 +6,15 @@ from optparse import OptionParser
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
-# from keras.layers.pooling import MaxPooling2D
-from keras.models import Model
 
+def setup_data(data_lines, path, data_group_folder):
+    """
 
-def extract_data(data_lines, path):
+    :param data_lines:
+    :param path:
+    :param data_group_folder: training_data, training_data_backwards, training_data_correction
+    :return:
+    """
     orientation_map = {'center': 0, 'left': 1, 'right': 2}
 
     images = []
@@ -28,9 +32,9 @@ def extract_data(data_lines, path):
             steering_angle_left = steering_angle_center + correction
             steering_angle_right = steering_angle_center - correction
 
-            img_center = process_image(path, line[orientation_map['center']])
-            img_left = process_image(path, line[orientation_map['left']])
-            img_right = process_image(path, line[orientation_map['right']])
+            img_center = process_image(path, data_group_folder, line[orientation_map['center']])
+            img_left = process_image(path, data_group_folder, line[orientation_map['left']])
+            img_right = process_image(path, data_group_folder, line[orientation_map['right']])
 
             # add images and angles to data set
             images.extend([img_center, img_left, img_right])
@@ -45,22 +49,33 @@ def extract_data(data_lines, path):
     X_train = np.concatenate([images, augmented_images])
     y_train = np.concatenate([steering_angles, augmented_steering_angle_list])
 
+    print('{0}: # of data lines: {1}, # of items after left and right: {2}, # after flipping: {3}'.format(
+                                                                                        data_group_folder,
+                                                                                        len(data_lines),
+                                                                                        len(images),
+                                                                                        len(X_train)))
+
     return X_train, y_train
 
-def process_image(path, file_path):
+def process_image(path, data_group_folder, file_path):
     """
-    Image augmentation
-    :param img_data:
+
+    :param path:
+    :param data_group_folder: training_data, training_data_backwards, training_data_correction
+    :param file_path:
     :return:
     """
     filename = file_path.split('/')[-1]
-    current_path = os.path.join(path, os.path.join(r'training_data/IMG/', filename))
+    current_path = os.path.join(path, os.path.join(r'{0}/IMG/'.format(data_group_folder), filename))
     image = cv2.imread(current_path)
     # Convert to RGB
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 def run(location):
+    # ---------------------------------------
+    # Setup paths
+    # ---------------------------------------
     if location == 'home':
         PATH = r'/Users/ericlok/Downloads'
     elif location == 'work':
@@ -68,40 +83,63 @@ def run(location):
     else: # AWS
         PATH = r'/home/carnd/CarND-Behavioral-Cloning-P3'
 
-    lines = []
-    with open(os.path.join(PATH, 'training_data/driving_log.csv')) as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            lines.append(line)
+    groups_of_data = ['training_data', 'training_data_backwards', 'training_data_correction']
+    image_list = []
+    steering_angle_list = []
 
-    del (lines[0])  # Remove the header (center, left, right, steering, throttle, brake, speed)
+    for data_group_folder in groups_of_data:
 
-    # Extract image and measurements from files
-    images, steering_angles = extract_data(lines, PATH)
+        lines = []
+        with open(os.path.join(PATH, '{0}/driving_log.csv'.format(data_group_folder))) as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                lines.append(line)
 
-    X_train = np.array(images)
-    y_train = np.array(steering_angles)
+        print('{0}: number of raw data: {1}'.format(data_group_folder, len(lines)))
 
+        if location == 'work':
+            del (lines[0])  # Remove the header (center, left, right, steering, throttle, brake, speed)
 
+        # ---------------------------------------
+        # Extract image and measurements from files
+        # ---------------------------------------
+        images, steering_angles = setup_data(lines, PATH, data_group_folder)
+        image_list.append(images)
+        steering_angle_list.append(steering_angles)
 
+        print('{0}: number of raw data after processing: {1}'.format(data_group_folder, len(images)))
+
+    X_train = np.concatenate(image_list)
+    y_train = np.concatenate(steering_angle_list)
+    print('Total number of data points: {0}'.format(len(X_train)))
+
+    # ---------------------------------------
+    # Construct the Model
+    # ---------------------------------------
     model = Sequential()
+    # Normalize the data. Input is 160 x 320 with 3 dimensions.
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    # Crop the top by 70 and bottom by 25. (begin width, end width) (begin height, end height)
     model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(Dropout(0.5))
+    # Apply a 5x5 convolution with 24 output filters, 2x2 stride, and relu activation
+    model.add(Convolution2D(nb_filter=24, nb_row=5, nb_col=5, subsample=(2, 2), activation='relu'))
+    # Apply a 5x5 convolution with 36 output filters, 2x2 stride, and relu activation
+    model.add(Convolution2D(nb_filter=36, nb_row=5, nb_col=5, subsample=(2, 2), activation='relu'))
+    # Apply a 5x5 convolution with 48 output filters, 2x2 stride, and relu activation
+    model.add(Convolution2D(nb_filter=48, nb_row=5, nb_col=5, subsample=(2, 2), activation='relu'))
+    # Apply a 3x3 convolution with 64 output filters, and relu activation
+    model.add(Convolution2D(nb_filter=64, nb_row=3, nb_col=3, activation='relu'))
+    # Apply a 3x3 convolution with 64 output filters, and relu activation
+    model.add(Convolution2D(nb_filter=64, nb_row=3, nb_col=3, activation='relu'))
+    model.add(Dropout(p=0.5))
     model.add(Flatten())
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dense(10))
-    model.add(Dense(1))
+    model.add(Dense(output_dim=100))
+    model.add(Dense(output_dim=50))
+    model.add(Dense(output_dim=10))
+    model.add(Dense(output_dim=1))
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
-
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=15)
     model.save('model.h5')
 
 if __name__ == '__main__':
